c95a6840c0223cad2348d136f2d786f645c92253
brain-hack <57687303+brain-hack@users.noreply.github.com>
2023-07-31 15:04:08 +0900

    Feature/login (#6)
    * impliment login
    
    * impliment logout
    
    ---------
    
    Co-authored-by: Geoff Yoon <koock1994@gmail.com>

--- src/main/java/com/mygroup/sbb/SecurityConfig.java
+++ src/main/java/com/mygroup/sbb/SecurityConfig.java
@@ -9,6 +9,10 @@ import org.springframework.security.web.header.writers.frameoptions.XFrameOption
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+
+
 
 @Configuration
 @EnableWebSecurity
@@ -23,6 +27,13 @@ public class SecurityConfig {
             .headers((headers) -> headers
                 .addHeaderWriter(new XFrameOptionsHeaderWriter(
                     XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+            .formLogin((formLogin) -> formLogin
+                .loginPage("/user/login")
+                .defaultSuccessUrl("/"))
+            .logout((logout) -> logout
+                .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true))
         ;
         return http.build();
     }
@@ -30,5 +41,10 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
